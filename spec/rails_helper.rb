3b6fa6d12c5c096c9969b1b01c34569a99f5c2bc
Your Name <you@example.com>
2025-05-10 21:35:08 +0900

    学習ログ投稿機能を作成

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,4 +71,5 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
   config.include FactoryBot::Syntax::Methods
   config.include Devise::Test::IntegrationHelpers, type: :system # 追加
+  config.include Devise::Test::IntegrationHelpers, type: :request # 追加
 end
